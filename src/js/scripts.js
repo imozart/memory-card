640be23db866442a3a347d22e2d25d2871834667
Mozart Iago <mozart.iago@gmail.com>
2020-08-19 00:31:21 -0300

    memory card game to studying pure js and css

---
+++ src/js/scripts.js
@@ -0,0 +1,55 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) {return;}
+    if (this === firstCard) {return;}
+
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    hasFlippedCard = lockBoard = false;
+    firstCard = secondCard = null
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        card.style.order = String(Math.floor(Math.random() * cards.length));
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
